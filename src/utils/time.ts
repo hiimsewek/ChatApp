b97a09292dd14f57f607de987e392e2a7d2b49aa
hiimsewek <102280@stud.uz.zgora.pl>
2023-09-05 23:54:47 +0200

    Add utility functions to format time

---
+++ src/utils/time.ts
@@ -0,0 +1,27 @@
+import dayjs from "dayjs";
+import dayjsIsToday from "dayjs/plugin/isToday";
+
+dayjs.extend(dayjsIsToday);
+
+export const hasWeekPassedFromNow = (date: Date) => {
+  const dateWithoutTime = dayjs(date).format("YYYY-MM-DD");
+  const diff = dayjs().diff(dateWithoutTime, "day");
+
+  return diff >= 7;
+};
+
+export const isPastYear = (date: Date) => dayjs().isAfter(date, "year");
+
+export const isToday = (date: Date) => dayjs(date).isToday();
+
+export const formatTime = (date: Date) => {
+  if (isToday(date)) {
+    return dayjs(date).format("HH:mm");
+  } else if (!hasWeekPassedFromNow(date)) {
+    return dayjs(date).format("ddd");
+  } else if (isPastYear(date)) {
+    return dayjs(date).format("D MMM YYYY");
+  } else {
+    return dayjs(date).format("D MMM");
+  }
+};
