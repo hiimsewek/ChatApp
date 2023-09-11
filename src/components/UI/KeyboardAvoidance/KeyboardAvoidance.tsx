3c373afbc9df62c65f0cf31de967d6c978e0e2da
hiimsewek <102280@stud.uz.zgora.pl>
2023-09-11 17:48:08 +0200

    Fix KeyboardAvoidance component on iOS

--- src/components/UI/KeyboardAvoidance/KeyboardAvoidance.tsx
+++ src/components/UI/KeyboardAvoidance/KeyboardAvoidance.tsx
@@ -6,20 +6,24 @@ import {
 } from "react-native";
 import React, { PropsWithChildren } from "react";
 import styles from "./styles";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 type KeyboardAvoidanceProps = PropsWithChildren<{
   style?: StyleProp<ViewStyle>;
 }>;
 
-const FormWrapper = ({ children, style }: KeyboardAvoidanceProps) => {
+const KeyboardAvoidance = ({ children, style }: KeyboardAvoidanceProps) => {
+  const headerHeight = useHeaderHeight();
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={[styles.stretch, style]}
+      keyboardVerticalOffset={headerHeight}
     >
       {children}
     </KeyboardAvoidingView>
   );
 };
 
-export default FormWrapper;
+export default KeyboardAvoidance;
